a5e62b869267084a88990f9eefcfae3af6747aaa
EdwinBuitrago <ebuitrago7@hotmail.com>
2020-11-19 08:40:26 -0500

    Code from local

---
+++ script.js
@@ -0,0 +1,70 @@
+var computerGuess;
+var userGuessLog = [];
+var attempts = 0;
+var maxGuesses = 10;
+
+function gameEnded() {
+    document.getElementById('newGameButton').style.display = 'inline-block';
+    document.getElementById('easyBtn').style.display ='none';
+    document.getElementById('hardBtn').style.display ='none';
+    document.getElementById('inputBox').setAttribute('readonly','readonly');
+    document.getElementById('inputBox').innerHTML = "";
+}
+
+function easyMode() {
+    maxGuesses = 10;
+    document.getElementById('easyBtn').className = 'activeButton';
+    document.getElementById('hardBtn').className = '';
+}
+
+function hardMode(){
+    maxGuesses = 5;
+    document.getElementById('hardBtn').className = 'activeButton';
+    document.getElementById('easyBtn').className = '';
+}
+
+function newGame() {
+    window.location.reload();
+}
+
+function init() {
+    computerGuess = Math.floor(Math.random() * 100 + 1);
+    //console.log (computerGuess);
+    document.getElementById('newGameButton').style.display = 'none';
+}
+
+function compareGuess(){
+    var userGuess = " " + document.getElementById("inputBox").value;
+    //console.log(userGuess);
+
+ userGuessLog.push(userGuess);
+ //console.log(userGuessLog);   
+ document.getElementById('guessLog').innerHTML = userGuessLog;
+ 
+ attempts++;
+ document.getElementById('attempts').innerHTML = ' ' + attempts;
+
+if(userGuessLog.length < maxGuesses) {
+    if(userGuess > computerGuess) {
+        document.getElementById('textOutput').innerHTML = 'Your guess is too high';
+        document.getElementById('inputBox').value = "";
+     } else if ( userGuess < computerGuess) {
+        document.getElementById('textOutput').innerHTML = 'Your guess is too low';
+        document.getElementById('inputBox').value = "";
+        } else {
+            document.getElementById('textOutput').innerHTML = 'Correct!';
+            gameEnded();
+        }
+    } else {
+        if(userGuess > computerGuess) {
+            document.getElementById('textOutput').innerHTML = 'You lose';
+            gameEnded();
+        } else if (userGuess < computerGuess) {
+            document.getElementById('textOutput').innerHTML = 'You lose';
+            gameEnded();
+        } else {
+            document.getElementById('textOutput').innerHTML = 'Correct!'
+            gameEnded();
+        }
+    }
+}
